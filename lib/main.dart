fadad9835eb07337b04a9817c6ce133cee78ab79
IT21185298 <it21185298@my.sliit.lk>
2024-03-06 20:52:14 +0530

    images and fonts add

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:ecotrack/screen/startPage.dart';
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+
+void main() => runApp(
+  DevicePreview(
+    //enabled: !kReleaseMode,
+    builder: (context) => MainApp(), // Wrap your app
+  ),
+);
+class MainApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StartPage(),
+    );
+  }
+}
