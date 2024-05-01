cf977cdc99d6ddaf67e68d95dff44025f783f9e0
IT21185298 <it21185298@my.sliit.lk>
2024-05-01 12:39:43 +0530

    admin home page update

--- lib/screen/Admin/adminHomePage.dart
+++ lib/screen/Admin/adminHomePage.dart
@@ -5,165 +5,123 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class AdminHomePage extends StatelessWidget {
-  const AdminHomePage({super.key});
+  const AdminHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const Row(
+        title: const Row(
           children: [
-            Text("Hello",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
-            SizedBox(width: 5,),
-            Text("Admin",style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold,fontSize: 25),)
+            Text(
+              "Hello",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+            SizedBox(width: 5),
+            Text(
+              "Admin",
+              style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25),
+            )
           ],
-        ) ,
+        ),
       ),
       body: Column(
         children: [
-          Padding(
-            padding: const EdgeInsets.only(right: 10,left: 10),
-            child: SizedBox(
-              width: 350,
-              height: 100,
-              child: ElevatedButton(
-                onPressed: (){
-                  // ignore: non_constant_identifier_names
-                  //Navigator.push(context, MaterialPageRoute(builder: (Context)=>const AdminPostPage()));
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)
-                  ),
-                  backgroundColor: Colors.grey[300]
-                ),
-                child: const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 20),
-                      // child: SvgPicture.asset('asset/Icons/SVG/complaint-svgrepo-com.svg',width: 80,height:80,),
-                    ),
-                    Text("Add Post",style: TextStyle(color: Colors.black,fontSize: 25),)
-                  ],
-                ),
-              ),
-            ),
+          buildButton(
+            context,
+            'Add Post',
+            'asset/Icons/SVG/complaint-svgrepo-com.svg', // Add your icon path
+            () {
+              // Add your functionality here
+            },
           ),
-          const SizedBox(height: 15,),
-          Padding(
-            padding: const EdgeInsets.only(right: 10,left: 10),
-            child: SizedBox(
-              width: 350,
-              height: 100,
-              child: ElevatedButton(
-                onPressed: (){
-                  // ignore: non_constant_identifier_names
-                  Navigator.push(context, MaterialPageRoute(builder: (Context)=>const AddDisposalPlaces()));
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)
-                  ),
-                  backgroundColor: Colors.grey[300]
-                ),
-                child: const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 20),
-                      // child: SvgPicture.asset('asset/Icons/SVG/complaint-svgrepo-com.svg',width: 80,height:80,),
-                    ),
-                    Text("Add Disposal Point",style: TextStyle(color: Colors.black,fontSize: 25),)
-                  ],
-                ),
-              ),
-            ),
+          SizedBox(height: 15),
+          buildButton(
+            context,
+            'Add Disposal Point',
+            'asset/Icons/SVG/complaint-svgrepo-com.svg', // Add your icon path
+            () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const AddDisposalPlaces()));
+            },
           ),
-          const SizedBox(height: 15,),
-          Padding(
-            padding: const EdgeInsets.only(right: 10,left: 10),
-            child: SizedBox(
-              width: 350,
-              height: 100,
-              child: ElevatedButton(
-                onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context)=>const AddTruckDriver()));
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)
-                  ),
-                  backgroundColor: Colors.grey[300]
-                ),
-                child: const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 20),
-                      // child: SvgPicture.asset('asset/Icons/SVG/complaint-svgrepo-com.svg',width: 80,height:80,),
-                    ),
-                    Text("Add Truck Driver",style: TextStyle(color: Colors.black,fontSize: 25),)
-                  ],
-                ),
-              ),
-            ),
+          SizedBox(height: 15),
+          buildButton(
+            context,
+            'Add Truck Driver',
+            'asset/Icons/SVG/complaint-svgrepo-com.svg', // Add your icon path
+            () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const AddTruckDriver()));
+            },
           ),
-          const SizedBox(height: 15,),
-          Padding(
-            padding: const EdgeInsets.only(right: 10,left: 10),
-            child: SizedBox(
-              width: 350,
-              height: 100,
-              child: ElevatedButton(
-                onPressed: (){
-                  
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)
-                  ),
-                  backgroundColor: Colors.grey[300]
-                ),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      // child: SvgPicture.asset('asset/Icons/SVG/complaint-svgrepo-com.svg',width: 80,height:80,),
-                    ),
-                    const Text("View Complain",style: TextStyle(color: Colors.black,fontSize: 25),)
-                  ],
-                ),
-              ),
-            ),
+          SizedBox(height: 15),
+          buildButton(
+            context,
+            'View Complain',
+            'asset/Icons/SVG/complaint-svgrepo-com.svg', // Add your icon path
+            () {
+              // Add your functionality here
+            },
           ),
-         const SizedBox(height: 15,),
-          Padding(
-            padding: const EdgeInsets.only(right: 10,left: 10),
-            child: SizedBox(
-              width: 350,
-              height: 100,
-              child: ElevatedButton(
-                onPressed: (){
-                  
-                },
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8)
-                  ),
-                  backgroundColor: Colors.grey[300]
-                ),
-                child: Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(left: 20),
-                      // child: SvgPicture.asset('asset/Icons/SVG/complaint-svgrepo-com.svg',width: 80,height:80,),
-                    ),
-                    const Text("Edit Profile Request",style: TextStyle(color: Colors.black,fontSize: 25),)
-                  ],
-                ),
-              ),
-            ),
+          SizedBox(height: 15),
+          buildButton(
+            context,
+            'Edit Profile Request',
+            'asset/Icons/SVG/complaint-svgrepo-com.svg', // Add your icon path
+            () {
+              // Add your functionality here
+            },
           ),
         ],
       ),
     );
   }
+
+  Widget buildButton(
+  BuildContext context,
+  String text,
+  String iconPath,
+  VoidCallback onPressed,
+) {
+  return Padding(
+    padding: const EdgeInsets.only(right: 10, left: 10),
+    child: SizedBox(
+      width: 350,
+      height: 100,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+          backgroundColor: Colors.white, // Change background color to white
+          elevation: 5, // Add elevation for shadow
+          shadowColor: Colors.grey, // Set shadow color
+        ),
+        child: Row(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(left: 20),
+              child: SvgPicture.asset(
+                iconPath,
+                width: 50, // Adjust icon size as needed
+                height: 50, // Adjust icon size as needed
+              ),
+            ),
+            SizedBox(width: 20), // Adjust space between icon and text
+            Text(
+              text,
+              style: TextStyle(color: Colors.black, fontSize: 20), // Adjust font size and color
+            )
+          ],
+        ),
+      ),
+    ),
+  );
 }
+}
+
