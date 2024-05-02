09f0a320c892f5f6303ea4b1304411debac92c16
IT21185298 <it21185298@my.sliit.lk>
2024-05-02 07:53:59 +0530

    profile update

--- lib/screen/User/userProfile.dart
+++ lib/screen/User/userProfile.dart
@@ -10,6 +10,54 @@ class UserProfile extends StatefulWidget {
 class _UserProfileState extends State<UserProfile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dilsha Madushanka"),
+      ),
+      body:Container(
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children:  [
+            Divider(),
+            ListTile(
+              leading: Icon(Icons.edit_attributes,color: Colors.blue,),
+              title: Text('Edit Profile'),
+              subtitle: Text("change youe personal details"),
+              trailing: Icon(Icons.menu),
+              onTap: (){
+              },
+            ),
+            Divider(),
+             ListTile(
+              leading: Icon(Icons.sell,color: Colors.green,),
+              title: const Text('Sell'),
+              subtitle: Text("Sell Your Iteams"),
+              trailing: Icon(Icons.menu,),
+              onTap: (){
+              },
+            ),
+             Divider(),
+             ListTile(
+              leading: Icon(Icons.report,color: Colors.red,),
+              title: Text('Report'),
+              subtitle: Text("Check your selling details"),
+              trailing: Icon(Icons.menu),
+              onTap: (){
+              },
+            ),
+            Divider(),
+             ListTile(
+              leading: Icon(Icons.message,color: Colors.lime,),
+              title: Text('Complain'),
+              subtitle: Text("submit your complain "),
+              trailing: Icon(Icons.menu),
+              onTap: (){
+              },
+            ),
+           
+          ],
+        ),
+      ) ,
+    );
   }
 }
