3330ebb0a609662e2a43ea2ca7ded63d2f9b69ec
IT21185298 <it21185298@my.sliit.lk>
2024-05-04 12:58:38 +0530

    sell item validation

--- lib/screen/User/sellItems.dart
+++ lib/screen/User/sellItems.dart
@@ -17,6 +17,7 @@ class SellItems extends StatefulWidget {
 }
 
 class _SellItemsState extends State<SellItems> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   late TextEditingController _nameController;
   late TextEditingController _quantityController;
   late TextEditingController _priceController;
@@ -41,76 +42,103 @@ class _SellItemsState extends State<SellItems> {
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            _imageFile != null ? Image.file(_imageFile!) : const Icon(Icons.image),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                _pickImage();
-              },
-              child: const Text('Pick Image'),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Name',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            TextFormField(
-              controller: _nameController,
-              decoration: const InputDecoration(
-                hintText: 'Enter name',
-                border: OutlineInputBorder(),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _imageFile != null ? Image.file(_imageFile!) : const Icon(Icons.image),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  _pickImage();
+                },
+                child: const Text('Pick Image'),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Quantity',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            TextFormField(
-              controller: _quantityController,
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                hintText: 'Enter quantity',
-                border: OutlineInputBorder(),
+              const SizedBox(height: 20),
+              const Text(
+                'Name',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Price',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            TextFormField(
-              controller: _priceController,
-              keyboardType: const TextInputType.numberWithOptions(decimal: true),
-              decoration: const InputDecoration(
-                hintText: '0.00',
-                border: OutlineInputBorder(),
+              TextFormField(
+                controller: _nameController,
+                decoration: const InputDecoration(
+                  hintText: 'Enter name',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a name';
+                  }
+                  return null;
+                },
               ),
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Description',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            TextFormField(
-              controller: _descriptionController,
-              maxLines: null,
-              decoration: const InputDecoration(
-                hintText: 'Enter description',
-                border: OutlineInputBorder(),
+              const SizedBox(height: 20),
+              const Text(
+                'Quantity',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-            ),
-            const SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: _imageFile != null ? _submitData : null,
-                child: const Text('Submit'),
+              TextFormField(
+                controller: _quantityController,
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  hintText: 'Enter quantity',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a quantity';
+                  }
+                  return null;
+                },
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              const Text(
+                'Price',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                controller: _priceController,
+                keyboardType: const TextInputType.numberWithOptions(decimal: true),
+                decoration: const InputDecoration(
+                  hintText: 'Enter price',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a price';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Description',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              TextFormField(
+                controller: _descriptionController,
+                maxLines: null,
+                decoration: const InputDecoration(
+                  hintText: 'Enter description',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a description';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: _imageFile != null ? _submitData : null,
+                  child: const Text('Submit'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -127,52 +155,55 @@ class _SellItemsState extends State<SellItems> {
   }
 
   Future<void> _submitData() async {
-    if (_nameController.text.isEmpty ||
-        _quantityController.text.isEmpty ||
-        _priceController.text.isEmpty ||
-        _descriptionController.text.isEmpty) {
-      return;
-    }
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
 
-    var request = http.MultipartRequest(
-      'POST',
-      Uri.parse('$localhost/users/store_items'), // Replace with your actual backend URL
-    );
-
-    if (_imageFile != null) {
-      var imageFile = await http.MultipartFile.fromPath(
-        'imagePath',
-        _imageFile!.path,
+      // Prepare data for submission
+      var request = http.MultipartRequest(
+        'POST',
+        Uri.parse('$localhost/users/store_items'), // Replace with your actual backend URL
       );
-      request.files.add(imageFile);
-    }
 
-    request.fields['name'] = _nameController.text;
-    request.fields['quantity'] = _quantityController.text;
-    request.fields['price'] = _priceController.text;
-    request.fields['description'] = _descriptionController.text;
-
-    request.headers['Authorization'] = 'Bearer ${widget.token}';
-    request.headers['VERSION'] = 'V1';
-
-    try {
-      var response = await request.send();
-      if (response.statusCode == 201) {
-        setState(() {
-          _imageFile = null;
-          _priceController.clear();
-          _nameController.clear();
-          _quantityController.clear();
-          _descriptionController.clear();
-        });
-        _showSuccessDialog();
-      } else {
-        print('Error uploading data: ${response.reasonPhrase}');
-        final responseBody = await response.stream.bytesToString();
-        print('Response body: $responseBody');
+      // Add image file to the request if available
+      if (_imageFile != null) {
+        var imageFile = await http.MultipartFile.fromPath(
+          'imagePath',
+          _imageFile!.path,
+        );
+        request.files.add(imageFile);
       }
-    } catch (e) {
-      print('Error submitting data: $e');
+
+      // Add form data to the request
+      request.fields['name'] = _nameController.text;
+      request.fields['quantity'] = _quantityController.text;
+      request.fields['price'] = _priceController.text;
+      request.fields['description'] = _descriptionController.text;
+
+      // Add authorization token to headers
+      request.headers['Authorization'] = 'Bearer ${widget.token}';
+      request.headers['VERSION'] = 'V1';
+
+      try {
+        var response = await request.send();
+        if (response.statusCode == 201) {
+          // Clear form fields and show success dialog on successful submission
+          setState(() {
+            _imageFile = null;
+            _priceController.clear();
+            _nameController.clear();
+            _quantityController.clear();
+            _descriptionController.clear();
+          });
+          _showSuccessDialog();
+        } else {
+          // Handle error cases
+          print('Error uploading data: ${response.reasonPhrase}');
+          final responseBody = await response.stream.bytesToString();
+          print('Response body: $responseBody');
+        }
+      } catch (e) {
+        print('Error submitting data: $e');
+      }
     }
   }
 
